36c41bdeba5370a8f5bfb38409901717fcc54537
Cera <cerasamson@gmail.com>
2020-06-24 21:47:58 -0700

    import bootstrap

---
+++ src/components/CollapsibleData.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class CollapsibleData extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          open: false
+      }
+  }  
+}
